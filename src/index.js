7486ce15db00435217c707bbc50d695044f5e930
Tom Joseph <swashedbuckles@gmail.com>
2019-10-15 00:19:46 -0400

    basic functionality of guard

--- src/index.js
+++ src/index.js
@@ -6,6 +6,27 @@ function guard(value, types) {
   if(types == null || Array.isArray(types) && types.length === 0) {
     return;
   }
+
+  const toCheck = [].concat(types);
+  const errorIndex = toCheck.reduce((res, type, idx) => {
+    if(res > -1) {
+      return res;
+    }
+
+    if(typeof type === 'string' && typeof value !== type) {
+      return idx;
+    }
+
+    if(!(value instanceof type)) {
+      return idx;
+    }
+
+    return res;
+  }, -1);
+
+  if(errorIndex  > -1) {
+    throw new TypeError(`Expected ${value} to be a ${types[errorIndex]}`);
+  }
 }
 
 module.exports = guard;
